9a487f6835616423e24e5595906322ae284343b0
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2024-04-13 23:30:44 +0530

    implement sockets communication

---
+++ frontend/src/app/Service/socket.service.ts
@@ -0,0 +1,113 @@
+import { Injectable } from '@angular/core';
+import { Socket, io } from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SocketService {
+  IsAdmin = 0;
+  localStream: MediaStream = new MediaStream();
+  remoteStream: MediaStream = new MediaStream();
+  stunServers: any = {
+    iceServers: [
+      {
+        urls: [
+          'stun:stun1.l.google.com:19302',
+          'stun:stun2.l.google.com:19302',
+        ],
+      },
+    ],
+    iceCandidatePoolSize: 10,
+  };
+  peerconnection: RTCPeerConnection = new RTCPeerConnection(this.stunServers);
+  webSocket: Socket;
+  SDP1: any;
+
+  constructor() {
+    this.webSocket = io('http://localhost:4000/');
+    this.webSocket.on('ReceiveOffer', (data) => {
+      if (this.IsAdmin == 0) {
+        this.createAnswer(data);
+      }
+    });
+    this.webSocket.on('ReceivedAnswer', (Answer) => {
+      console.log('answer received');
+      if (this.IsAdmin == 1) {
+        try {
+          this.peerconnection.setRemoteDescription(
+            new RTCSessionDescription(JSON.parse(Answer))
+          );
+          this.localStream.getTracks().forEach((track) => {
+            this.peerconnection.addTrack(track, this.localStream);
+          });
+          this.peerconnection.ontrack = (event) => {
+            event.streams[0].getTracks().forEach((track) => {
+              this.remoteStream.addTrack(track);
+            });
+          };
+        } catch (Exception) {
+          console.log(Exception);
+        }
+      }
+    });
+  }
+  async createAnswer(sdp: any) {
+    this.peerconnection.onicecandidate = async (event) => {
+      if (event.candidate) {
+        console.log('Adding answer candidate...:', event.candidate);
+      }
+    };
+    await this.peerconnection.setRemoteDescription(JSON.parse(sdp));
+    let answer = await this.peerconnection.createAnswer();
+    await this.peerconnection.setLocalDescription(answer);
+    this.webSocket.emit(
+      'AnswerCreated',
+      JSON.stringify(this.peerconnection.localDescription)
+    );
+  }
+
+  async getUserMedia(): Promise<MediaStream[]> {
+    this.IsAdmin = 0;
+    this.localStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    });
+    this.remoteStream = new MediaStream();
+    this.localStream.getTracks().forEach((track) => {
+      this.peerconnection.addTrack(track, this.localStream);
+    });
+    this.peerconnection.ontrack = (event) => {
+      event.streams[0].getTracks().forEach((track) => {
+        this.remoteStream.addTrack(track);
+      });
+    };
+    this.peerconnection.onicecandidate = async (event) => {
+      if (event.candidate) {
+        this.webSocket.emit(
+          'OfferCreated',
+          JSON.stringify(this.peerconnection.localDescription)
+        );
+      }
+    };
+    let offer = await this.peerconnection.createOffer();
+    await this.peerconnection.setLocalDescription(offer);
+    return [this.localStream, this.remoteStream];
+  }
+
+  async getUserMedia2(): Promise<MediaStream[]> {
+    this.localStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    });
+    this.remoteStream = new MediaStream();
+    this.localStream.getTracks().forEach((track) => {
+      this.peerconnection.addTrack(track, this.localStream);
+    });
+    this.peerconnection.ontrack = (event) => {
+      event.streams[0].getTracks().forEach((track) => {
+        this.remoteStream.addTrack(track);
+      });
+    };
+    return [this.localStream, this.remoteStream];
+  }
+}
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateSectionComponent } from './Component/create-section/create-section.component';
 import { HomeComponent } from './Component/home/home.component';
 import { ConnectComponent } from './Component/connect/connect.component';
+import { Approach2Component } from './Component/approach2/approach2.component';
+import { Client1Component } from './Component/client1/client1.component';
+import { Client2Component } from './Component/client2/client2.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'section', component: CreateSectionComponent },
-  { path: 'connect', component: ConnectComponent }
+  { path: 'connect', component: ConnectComponent },
+  { path: 'alternateapproach', component: Approach2Component },
+  {path:'client1',component:Client1Component},
+  {path:'client2',component:Client2Component},
 ];
 
 @NgModule({
